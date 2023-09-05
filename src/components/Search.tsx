70afc6bc3ce31de040d7cc24a88f943a6061290f
nicKondratiev <kondratievinikoloz@gmail.com>
2023-09-06 00:09:06 +0400

    import carsData to Search.tsx and create drop down for search filtering

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,19 +1,36 @@
-import { useState } from "react";
+import UseToggle from "../hooks/UseToggle";
+import { MouseEventHandler } from "react";
+import carsData from "../db.json";
 
 const Search = () => {
-  const [toggle, setToggle] = useState<boolean>(false);
+  const [manufacturer, toggleManufacturer] = UseToggle(false);
 
-  console.log(toggle);
+  const handleClick: MouseEventHandler<HTMLDivElement> = () => {
+    toggleManufacturer();
+  };
+
+  carsData.map((car) => console.log(car.brand));
 
   return (
     <div className="text-2xl text-white">
       <div
-        onClick={() => setToggle(!toggle)}
+        onClick={handleClick}
         className="w-fit cursor-pointer rounded-xl bg-orange-700 px-3 py-2"
       >
         <h1>Manufacturer</h1>
       </div>
-      <div></div>
+
+      <div
+        className={`${
+          manufacturer ? "relative" : "hidden"
+        } h-[350px] w-[350px] overflow-scroll rounded-xl bg-white`}
+      >
+        {carsData.map((car, id) => (
+          <div className="text-black" key={id}>
+            <p>{car.brand}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
