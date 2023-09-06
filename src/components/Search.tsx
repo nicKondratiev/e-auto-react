5cc5460cd384935e5284a65a28cb8e7d6c966d4e
nicKondratiev <kondratievinikoloz@gmail.com>
2023-09-06 21:48:13 +0400

    create Location setter using reusable comp DropDown.tsx

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -4,7 +4,8 @@ import { MouseEventHandler, useState } from "react";
 import UseToggle from "../hooks/UseToggle";
 
 // json file
-import carsData from "../db.json";
+import carsData from "../carsData.json";
+import locationData from "../locations.json";
 
 // reusable component
 import DropDown from "./searchParam_comps/DropDown";
@@ -14,12 +15,15 @@ const Search = () => {
   // togglers
   const [toggleManu, setToggleManu] = UseToggle(false);
   const [toggleModels, setToggleModels] = UseToggle(false);
+  const [toggleLocations, setToggleLocations] = UseToggle(false);
 
   // setters
   const [manu, setManu] = useState<string>("");
   const [model, setModel] = useState<string>("");
+  const [location, setLocation] = useState<string>("");
   console.log(manu);
   console.log(model);
+  console.log(location);
 
   // toggle handlers
   const handleManu: MouseEventHandler<HTMLDivElement> = () => {
@@ -30,11 +34,15 @@ const Search = () => {
     setToggleModels();
   };
 
+  const handleLocations: MouseEventHandler<HTMLDivElement> = () => {
+    setToggleLocations();
+  };
+
   // we iterate over carsData and return only car manufacturers/brands
   const manufacturers = carsData.map((car) => car.brand);
   const selectedManu = carsData.filter((car) => car.brand === manu);
-
   const models = selectedManu[0]?.models;
+  const locations = locationData.georgia;
 
   return (
     <div>
@@ -51,6 +59,13 @@ const Search = () => {
           handleClick={handleModels}
           toggle={toggleModels}
           Child={<Child data={models} setItem={setModel} />}
+        />
+
+        <DropDown
+          header="Location"
+          handleClick={handleLocations}
+          toggle={toggleLocations}
+          Child={<Child data={locations} setItem={setLocation} />}
         />
       </div>
     </div>
