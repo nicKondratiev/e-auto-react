a8f436e85725d46b1918a2f577a1b454fc41d21e
nicKondratiev <kondratievinikoloz@gmail.com>
2023-09-08 21:28:41 +0400

    create handleNavigate function in Button.tsx

--- src/components/searchParam_comps/Button.tsx
+++ src/components/searchParam_comps/Button.tsx
@@ -9,12 +9,31 @@ const Button = () => {
   const model = store.searchParams.model.split(" ").join("-");
   const location = store.searchParams.location.split(" ").join("-");
 
+  const handleNavigate = () => {
+    // we push route params to this array if their value is truthy
+    const routeParams = [];
+
+    if (manu) {
+      routeParams.push(manu);
+    }
+
+    if (model) {
+      routeParams.push(model);
+    }
+
+    if (location) {
+      routeParams.push(location);
+    }
+
+    const url = `iyideba-manqanebi/${routeParams.join("/")}`;
+
+    navigate(url);
+  };
+
   return (
     <div className="col-start-4 row-start-3">
       <button
-        onClick={() =>
-          navigate(`/iyideba-manqanebi/${manu}/${model}/${location}`)
-        }
+        onClick={handleNavigate}
         className="w-2/3 rounded bg-orange-500 p-1"
       >
         Search
