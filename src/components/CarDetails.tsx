075c3440e8908ff022918cc96fdb92eb6422ce4d
nicKondratiev <kondratievinikoloz@gmail.com>
2023-09-09 18:56:38 +0400

    install json-server package & add script for running it

--- src/components/CarDetails.tsx
+++ src/components/CarDetails.tsx
@@ -1,5 +1,9 @@
 const CarDetails = () => {
-  return <div>CarDetails</div>;
+  return (
+    <div>
+      <div className="h-[400px] w-[600px] rounded-xl bg-white"></div>
+    </div>
+  );
 };
 
 export default CarDetails;
