b39703a14b050b8e5ffba63254c8c518e44b7650
nicKondratiev <kondratievinikoloz@gmail.com>
2023-09-14 20:21:42 +0400

    create the fetchCars function to estimate the number of search results

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,58 +1,78 @@
 import useStore from "../store";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useMemo, useState } from "react";
+
+type SearchParams = {
+  paramName: string;
+  paramValue: string;
+}[];
 
 const Button = () => {
   const navigate = useNavigate();
   const store = useStore();
+
+  // we pass this url state to navigate as an argument
+  const [url, setUrl] = useState<string>("");
+  // resCount equals to the amount of cars on sale (by specific filters)
+  const [resCount, setResCount] = useState<number>(0);
 
   // this function replaces empty spaces with dashes (to make string valid for url)
   const urlStringModifier = (value: string) => {
     return value.replace(" ", "-");
   };
 
-  const manu = {
-    paramName: "manu",
-    url: urlStringModifier(store.searchParams.manu),
-  };
-  const model = {
-    paramName: "model",
-    url: urlStringModifier(store.searchParams.model),
-  };
-  const location = {
-    paramName: "location",
-    url: urlStringModifier(store.searchParams.location),
-  };
+  const searchParams: SearchParams = useMemo(() => {
+    return [
+      {
+        paramName: "manu",
+        paramValue: urlStringModifier(store.searchParams.manu),
+      },
+      {
+        paramName: "model",
+        paramValue: urlStringModifier(store.searchParams.model),
+      },
+      {
+        paramName: "location",
+        paramValue: urlStringModifier(store.searchParams.location),
+      },
+    ];
+  }, [store.searchParams]);
 
   // this function checks if the searchParam item exists and pushes in the array if it does
   const routePusher = (
     routeParams: string[] = [],
-    str: { paramName: string; url: string }
+    param: { paramName: string; paramValue: string }
   ) => {
-    if (str.url) {
-      routeParams.push(`${str.paramName}=${str.url}`);
+    if (param.paramValue) {
+      routeParams.push(`${param.paramName}=${param.paramValue}`);
     }
   };
 
-  const handleNavigate = () => {
-    // we push route params to this array if their value is truthy
+  useEffect(() => {
     const routeParams: string[] = [];
 
-    routePusher(routeParams, manu);
-    routePusher(routeParams, model);
-    routePusher(routeParams, location);
+    searchParams.map((param) => routePusher(routeParams, param));
 
-    const url = `iyideba-manqanebi?${routeParams.join("&")}`;
+    const fetchCars = async () => {
+      const res = await axios.get(
+        `http://localhost:4000/carsOnSale?${routeParams.join("&")}`
+      );
+      setResCount(res.data.length);
+    };
+    fetchCars();
 
-    navigate(url);
-  };
+    const urlQueryParams = `iyideba-manqanebi?${routeParams.join("&")}`;
+    setUrl(urlQueryParams);
+  }, [searchParams]);
 
   return (
     <div className="col-start-4 row-start-3">
       <button
-        onClick={handleNavigate}
+        onClick={() => navigate(url)}
         className="w-2/3 rounded bg-orange-500 p-1"
       >
-        Search
+        Search ({resCount})
       </button>
     </div>
   );
