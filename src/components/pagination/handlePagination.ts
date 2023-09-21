bd79157732ee92aaf5a0d4a8e9ec09fca3e1a92c
nicKondratiev <kondratievinikoloz@gmail.com>
2023-09-21 13:25:51 +0400

    move pagination handler to a seperate module

---
+++ src/components/pagination/handlePagination.ts
@@ -0,0 +1,20 @@
+// import { useNavigate, useLocation } from "react-router-dom";
+// import { useSearchParams } from "react-router-dom";
+
+import { SetStateAction } from "react";
+
+export const handlePagination = (
+  page: number,
+  setPage: React.Dispatch<SetStateAction<number>>,
+  navigate: (url: string) => void,
+  location: Location,
+  params: URLSearchParams
+) => {
+  setPage(page);
+
+  params.set("page", page.toString());
+
+  const newURL = `${location.pathname}?${params.toString()}`;
+
+  navigate(newURL);
+};
