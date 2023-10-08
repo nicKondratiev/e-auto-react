8214c81c8ffb2b22e537dd9f4e22658b7af95b1e
nicKondratiev <kondratievinikoloz@gmail.com>
2023-10-08 20:39:09 +0400

    update code so value is removed whenever toogled

--- src/components/searchParam_comps/fuel/FuelChild.tsx
+++ src/components/searchParam_comps/fuel/FuelChild.tsx
@@ -18,12 +18,20 @@ const FuelChild = () => {
     return store.searchParams.fuelType === fuel;
   };
 
+  const itemSetter = (fuel: string) => {
+    if (isSelected(fuel)) {
+      store.addFuelType("");
+    } else {
+      store.addFuelType(fuel);
+    }
+  };
+
   return (
     <div className="p-4 text-base text-black">
       <div className="flex flex-wrap gap-[12px]">
         {fuelTypes.map((fuel, index) => (
           <label
-            onClick={() => store.addFuelType(fuel)}
+            onClick={() => itemSetter(fuel)}
             className={`${
               isSelected(fuel)
                 ? "border-green-500 bg-green-100"
